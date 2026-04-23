61c84ce48a56dad56b69e7e8462c7efd3d3bccb4
pauldavid23 <paul.david.gelario@gmail.com>
2026-04-23 23:57:29 +0800

    Add project files.

---
+++ C-sharpLeapYearCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter year"); 
+int year = Convert.ToInt32(Console.ReadLine());
+
+if (year % 4 == 0)
+{
+    if (year % 100 == 0)
+    {
+        if (year % 400 == 0)
+        {
+            Console.WriteLine(year + " is a leap year.");
+        }
+        else
+        {
+            Console.WriteLine(year + " is not a leap year.");
+        }
+    }
+    else
+    {
+        Console.WriteLine(year + " is a leap year.");
+    }
+}
+else
+{
+    Console.WriteLine(year + " is not a leap year.");
+}
